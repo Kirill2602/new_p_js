bd7b71fb61a2c1a353e80b467ac6d63e78008118
Kirill2602 <kaspicin2602@gmail.com>
2021-09-09 15:43:03 +0300

    commit

---
+++ src/audioArr.js
@@ -0,0 +1,22 @@
+import aud1 from './audio/3271_call-sound.ru__.mp3';
+import aud2 from './audio/3297_call-sound.ru__.mp3';
+import aud3 from './audio/3332_call-sound.ru__.mp3';
+import aud4 from './audio/3392_call-sound.ru__.mp3';
+export const music = [
+    {
+        title: 'Аудио №1',
+        src: aud1
+    },
+    {
+        title: 'Аудио №2',
+        src: aud2
+    },
+    {
+        title: 'Аудио №3',
+        src: aud3
+    },
+    {
+        title: 'Аудио №4',
+        src: aud4
+    }
+]
